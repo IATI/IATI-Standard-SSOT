7abafb05326c6b8ebe4667a8cb65449adbd3ca43
Ben Webb <bjwebb67@googlemail.com>
2013-10-11 15:14:08 +0100

    Pull in attributes from the schema

--- gen.py
+++ gen.py
@@ -1,10 +1,14 @@
 from lxml import etree as ET
 import string
 import os
+import textwrap
 
 
 namespaces = {
     'xsd': 'http://www.w3.org/2001/XMLSchema'
+}
+custom_attributes = {
+    'xml:lang': 'ISO 2 letter code specifying the language of text in this element.'
 }
 
 import re
@@ -13,6 +17,18 @@ class Schema2Doc(object):
     def __init__(self, schema):
         self.tree = ET.parse("./iati-schemas/"+schema)
         self.tree2 = ET.parse("./iati-schemas/iati-common.xsd")
+
+    def get_complexType(self, complexType_name):
+        complexType = self.tree.find("//xsd:complexType[@name='{0}']".format(complexType_name), namespaces=namespaces)
+        if complexType is None:
+            complexType = self.tree2.find("//xsd:complexType[@name='{0}']".format(complexType_name), namespaces=namespaces)
+        return complexType
+
+    def get_attribute(self, attribute_name):
+        attribute = self.tree.find("//xsd:attribute[@name='{0}']".format(attribute_name), namespaces=namespaces)
+        if attribute is None:
+            attribute = self.tree2.find("//xsd:attribute[@name='{0}']".format(attribute_name), namespaces=namespaces)
+        return attribute
 
     def get_element(self, element_name, path):
         element = self.tree.find("//xsd:element[@name='{0}']".format(element_name), namespaces=namespaces)
@@ -31,17 +47,23 @@ class Schema2Doc(object):
         with open('docs/'+path+element_name+'.rst', 'w') as fp:
             fp.write(element_name+'\n'+('='*len(element_name))+'\n\n')
             
-            #FIXME (This does not belong here)
+            fp.write('\nFrom the schema\n~~~~~~~~~~~~~~~\n\n')
+            fp.write(element.find(".//xsd:documentation", namespaces=namespaces).text)
+            fp.write('\n\n')
+
+            #FIXME (element_loop does not belong here)
+            attributes = self.attribute_loop(element)
+            fp.write('Attributes\n~~~~~~~~~~\n\n')
+            fp.write('\n'.join([ a[0]+'\n  '+textwrap.dedent(a[1]).strip().replace('\n','\n  ') for a in attributes ]))
+
+            #FIXME (element_loop does not belong here)
             childnames = self.element_loop(element, path)
             if childnames:
-                fp.write('Subelements\n~~~~~~~~~~~\n\n')
+                fp.write('\nSubelements\n~~~~~~~~~~~\n\n')
                 fp.write('.. toctree::\n   :titlesonly:\n\n')
                 fp.write('\n'.join([ '   '+element_name+'/'+c for c in childnames]))
                 fp.write('\n\n')
-
-            fp.write('From the schema\n~~~~~~~~~~~~~~~\n\n')
-            fp.write(element.find(".//xsd:documentation", namespaces=namespaces).text)
-            fp.write('\n\n')
+            
 
 
     def element_loop(self, element, path):
@@ -58,30 +80,48 @@ class Schema2Doc(object):
                 childnames.append(a['ref'])
         return childnames
 
-    def attribute_loop(self, element, indent=''):
-        if element.find("xsd:complexType[@mixed='true']", namespaces=namespaces) is not None:
-            print_column_info('text', indent)
+    def attribute_loop(self, element):
+        #if element.find("xsd:complexType[@mixed='true']", namespaces=namespaces) is not None:
+        #    print_column_info('text', indent)
             
         a = element.attrib
+        type_attributes = []
         if 'type' in a:
-            if a['type'] in ['plainType','textType','codeType','codeReqType','currencyType','dateType','xsd:anyURI','xsd:decimal']:
-                print_column_info('text', indent+'  ')
-                if a['type'] == 'codeReqType':
-                    print_column_info('code', indent+'  ', True)
-                if a['type'] == 'codeType':
-                    print_column_info('code', indent+'  ', False)
-                if a['type'] == 'currencyType':
-                    print_column_info('currency', indent+'  ', False)
-                    print_column_info('value-date', indent+'  ', False)
-                if a['type'] == 'dateType':
-                    print_column_info('iso-date', indent+'  ', False)
-            else: raise Exception, a['type']
+            complexType = self.get_complexType(a['type'])
+            if complexType is None:
+                print 'Notice: No attributes for', a['type']
+            else:
+                type_attributes = complexType.findall('xsd:attribute', namespaces=namespaces)
+            #print_column_info('text', indent+'  ')
+            """"
+            if a['type'] == 'codeReqType':
+                print_column_info('code', indent+'  ', True)
+            if a['type'] == 'codeType':
+                print_column_info('code', indent+'  ', False)
+            if a['type'] == 'currencyType':
+                print_column_info('currency', indent+'  ', False)
+                print_column_info('value-date', indent+'  ', False)
+            if a['type'] == 'dateType':
+                print_column_info('iso-date', indent+'  ', False)
+            """
 
+        out = []
         for attribute in (
             element.findall('xsd:complexType/xsd:attribute', namespaces=namespaces) +
-            element.findall('xsd:complexType/xsd:simpleContent/xsd:extension/xsd:attribute', namespaces=namespaces)
+            element.findall('xsd:complexType/xsd:simpleContent/xsd:extension/xsd:attribute', namespaces=namespaces) +
+            type_attributes
             ):
-            print_column_info( attribute.get('ref') or attribute.get('name'), indent, attribute.get('use') == 'required' )
+            if 'ref' in attribute.attrib:
+                if attribute.get('ref') in custom_attributes:
+                    out.append((attribute.get('ref'), custom_attributes[attribute.get('ref')]))
+                    continue
+                attribute = self.get_attribute(attribute.get('ref'))
+            doc = attribute.find(".//xsd:documentation", namespaces=namespaces)
+            if doc is not None:
+                out.append((attribute.get('name'), doc.text))
+            else:   
+                print 'Ack', ET.tostring(attribute)
+        return out
 
 activities = Schema2Doc('iati-activities-schema.xsd')
 activities.get_element('iati-activities', '')
