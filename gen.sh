3e9f1b0c463323cdd59f5c02b20ea197efff6d4f
Ben Webb <bjwebb67@googlemail.com>
2013-11-14 15:03:47 +0000

    Add some comments to gen.sh

--- gen.sh
+++ gen.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 set -o nounset
 
+# Remove docs (the output directory), and recreate directory structure
 rm -r docs
 mkdir docs
 mkdir docs/codelists
 cd IATI-Extra-Documentation || exit 1
 find -type d -exec mkdir ../docs/{} \;
 cd .. || exit 1
+
+# Generate documentation from the Schema etc
 python gen.py || exit 1
+
+# Append Extra-Documentation to the documentation we've just generated
 cd IATI-Extra-Documentation || exit 1
 find -type f -exec bash -c 'cat {} >> ../docs/{}' \;
 cd .. || exit 1
 
+# Generate documentation from codelists
 cd IATI-Codelists || exit 1
 ./gen.sh || exit 1
 cd .. || exit 1
@@ -42,6 +48,7 @@ View on github:
     " > docs/codelists/${fname}.rst;
 done
 
+# Build the documentation (generate html etc.) using sphinx
 cd docs || exit 1
 make html
 
