b2a6f9c24806f491c8b0d195914b80546ce7596b
Ben Webb <bjwebb67@googlemail.com>
2014-04-07 16:47:12 +0100

    [IATI/IATI-Websites#72] Generate XML sitemap

--- combined_gen.sh
+++ combined_gen.sh
@@ -20,6 +20,10 @@ done
 cd .. || exit 1
 ./gen_html.sh || exit $?
 
+echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > docs/en/_build/dirhtml/sitemap.xml
+find docs/en/_build/dirhtml | grep -v _static | grep index.html$ | sed 's|index.html$|</loc></url>|' | sed 's|docs/en/_build/dirhtml|<url><loc>http://dev.iatistandard.org|' >> docs/en/_build/dirhtml/sitemap.xml
+echo '</urlset>' >> docs/en/_build/dirhtml/sitemap.xml
+
 rm -rf docs-copy
 cp -r docs docs-copy
 
