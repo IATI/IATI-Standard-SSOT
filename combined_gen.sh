ac35f815333e95721e211d6ecb6458b552217b3f
Ben Webb <bjwebb67@googlemail.com>
2014-04-07 14:17:34 +0100

    [#34] Build combined documentation by copying instead of symlinks

--- combined_gen.sh
+++ combined_gen.sh
@@ -3,16 +3,19 @@
 ./gen_rst.sh || exit $?
 
 cd docs || exit 1
+
+mkdir en/developer
+cp ../../IATI-Developer-Documentation/*.rst en/developer
+cp -r ../../IATI-Developer-Documentation/*/ en/developer
+cp ../..//IATI-Guidance/en/*.rst en/
+cp -r ../../IATI-Guidance/en/*/ en/
+cp ../combined_sitemap.rst en/sitemap.rst
+
 git add .
 git commit -a -m 'Auto'
 git ls-tree -r --name-only HEAD | grep 'rst$' | while read filename; do
     echo $'\n\n\n'"*Last updated on $(git log -1 --format="%ad" --date=short -- $filename)*" >> $filename
 done
-
-ln -s /home/ssot/IATI-Developer-Documentation en/developer
-ln -s /home/ssot/IATI-Guidance/en/*.rst en/
-ln -s /home/ssot/IATI-Guidance/en/*/ en/
-cp ../special_sitemap.rst en/sitemap.rst
 
 cd .. || exit 1
 ./gen_html.sh || exit $?
