74a88307fe1379f2f6d18d796a89a30c4f43c3a1
Ben Webb <bjwebb67@googlemail.com>
2014-04-24 10:46:44 +0100

    [#39] Read sitemap URL from file

--- combined_gen.sh
+++ combined_gen.sh
@@ -21,7 +21,7 @@ cd .. || exit 1
 ./gen_html.sh || exit $?
 
 echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > docs/en/_build/dirhtml/sitemap.xml
-find docs/en/_build/dirhtml | grep -v _static | grep index.html$ | sed 's|index.html$|</loc></url>|' | sed 's|docs/en/_build/dirhtml|<url><loc>http://dev.iatistandard.org|' >> docs/en/_build/dirhtml/sitemap.xml
+find docs/en/_build/dirhtml | grep -v _static | grep index.html$ | sed 's|index.html$|</loc></url>|' | sed "s|docs/en/_build/dirhtml|<url><loc>http://`cat URL`|" >> docs/en/_build/dirhtml/sitemap.xml
 echo '</urlset>' >> docs/en/_build/dirhtml/sitemap.xml
 
 rm -rf docs-copy
