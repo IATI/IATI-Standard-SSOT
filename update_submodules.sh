6d4b12df3790aff06173199e268537d745a419d5
Dale Potter <dalepotter@users.noreply.github.com>
2015-09-25 12:44:16 +0100

    Fix typos

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,11 +1,11 @@
-# Script to update each of the 4 submodules in the IAST-SSOT repository
+# Script to update each of the 4 submodules in the IATI-SSOT repository
 # 
 
 for version in 1.04 1.05 2.01; do
-	# Checkout to teh specified version
+	# Checkout to the specified version
 	git checkout version-$version
 
-	# Pull each submodule for this versino
+	# Pull each submodule for this version
 	git submodule foreach git pull origin version-$version
 	
 	# Add each folder to staging
