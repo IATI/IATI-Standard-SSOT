c5ce658443c169f6e271de491ef0f2fce6d11184
Andy Lulham <a.lulham@gmail.com>
2018-11-20 15:22:45 +0000

    Add Language to list of incomplete codelists

--- gen.py
+++ gen.py
@@ -123,6 +123,7 @@ def is_complete_codelist(codelist_name):
         'HumanitarianScopeType',
         'HumanitarianScopeVocabulary',
         'IndicatorVocabulary',
+        'Language',
         'OrganisationIdentifier',
         'OrganisationRegistrationAgency'
     ]
